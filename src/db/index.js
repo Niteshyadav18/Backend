062a28f65e3c5981e00b28d29f66d1ab233ecf18
niteshyadav <125867866+Niteshyadav18@users.noreply.github.com>
2024-11-23 15:05:33 +0530

    utils code

--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,27 @@
-import mongoose, { mongo } from "mongoose";
+// Importing Mongoose library to interact with MongoDB
+import mongoose from "mongoose";
+
+// Importing a constant `DB_NAME` from another file that specifies the database name
 import { DB_NAME } from "../constant.js";
 
-
-const connectDB = async ()=>{
+// Defining an asynchronous function to connect to the MongoDB database
+const connectDB = async () => {
     try {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-    console.log(`MongoDB Connected !! DB HOST: ${connectionInstance.connection.host}`);
-    } catch (error) {
-        console.log("MONGODB CONNECTION ERROR: ",error);
-        process.exit(1)
-    }
-}
+        // Connect to MongoDB using the connection string from the environment variables and database name
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URI}/${DB_NAME}` // Combines the base URI and database name
+        );
 
-export default connectDB
+        // Log a success message when the connection is established
+        console.log(`MongoDB Connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        // Log the error message if the connection fails
+        console.log("MONGODB CONNECTION ERROR: ", error);
+
+        // Exit the Node.js process with a non-zero status code to indicate failure
+        process.exit(1);
+    }
+};
+
+// Export the `connectDB` function as the default export of this module
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is ruuning ar port : ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MONGO DB connection failed !!!",error)
+})
 
 
 
